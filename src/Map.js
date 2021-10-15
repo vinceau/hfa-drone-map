11515262e843b96746275e576291495f3efcd57d
Michael Ellis <anz5michael@gmail.com>
2021-10-16 00:04:39 +1100

    can draw lines

--- src/Map.js
+++ src/Map.js
@@ -7,13 +7,14 @@ mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_KEY;
 const Map = () => {
   const mapContainerRef = useRef(null);
 
+  const map = useRef(null);
   const [lng, setLng] = useState(5);
   const [lat, setLat] = useState(34);
-  const [zoom, setZoom] = useState(1.5);
+  const [zoom, setZoom] = useState(9);
 
   // Initialize map when component mounts
   useEffect(() => {
-    const map = new mapboxgl.Map({
+    map.current = new mapboxgl.Map({
       container: mapContainerRef.current,
       style: "mapbox://styles/mapbox/streets-v11",
       center: [lng, lat],
@@ -21,15 +22,76 @@ const Map = () => {
     });
 
     // Add navigation control (the +/- zoom buttons)
-    map.addControl(new mapboxgl.NavigationControl(), "top-right");
+    map.current.addControl(new mapboxgl.NavigationControl(), "top-right");
 
-    map.on("move", () => {
-      setLng(map.getCenter().lng.toFixed(4));
-      setLat(map.getCenter().lat.toFixed(4));
-      setZoom(map.getZoom().toFixed(2));
+    map.current.on("move", () => {
+      setLng(map.current.getCenter().lng.toFixed(4));
+      setLat(map.current.getCenter().lat.toFixed(4));
+      setZoom(map.current.getZoom().toFixed(2));
+    });
+    return () => map.remove();
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+
+  useEffect(() => {
+    console.log('hi')
+
+    map.current = new mapboxgl.Map({
+      container: mapContainerRef.current,
+      style: "mapbox://styles/mapbox/streets-v11",
+      center: [lng, lat],
+      zoom: zoom,
     });
 
-    // Clean up on unmount
+    // Add navigation control (the +/- zoom buttons)
+    map.current.addControl(new mapboxgl.NavigationControl(), "top-right");
+
+    map.current.on("move", () => {
+      setLng(map.current.getCenter().lng.toFixed(4));
+      setLat(map.current.getCenter().lat.toFixed(4));
+      setZoom(map.current.getZoom().toFixed(2));
+    });
+
+    const data = {
+      "type": "FeatureCollection",
+      "features": [
+          {
+              "type": "Feature",
+              "geometry": {
+                  "type": "LineString",
+                  "coordinates": [
+                      [
+                          -122.019807,
+                          45.632433
+                      ],
+                  ]
+              }
+          }
+      ]
+    }
+
+    map.current.on('load', async () => {
+      map.current.addSource('trace', { type: 'geojson', data: data});
+      map.current.addLayer({
+        'id': 'trace',
+        'type': 'line',
+        'source': 'trace',
+        'paint': {
+        'line-color': 'yellow',
+        'line-opacity': 0.75,
+        'line-width': 5
+        }
+      });
+      map.current.panTo(data.features[0].geometry.coordinates[0]);
+    })
+    
+    map.current.on('click', async (e) => {
+      const {lng, lat} = e.lngLat
+      console.log([lng, lat])
+      data.features[0].geometry.coordinates.push([lng, lat]);
+      map.current.getSource('trace').setData(data);
+    })
+
+
     return () => map.remove();
   }, []);
 
