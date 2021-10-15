553f35820274a7b5157ecd6c9201f513f8d9d6b4
Michael Ellis <anz5michael@gmail.com>
2021-10-16 00:32:50 +1100

    added line drawing

--- src/Map.js
+++ src/Map.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactMapboxGl, { Layer, Feature } from "react-mapbox-gl";
+import ReactMapboxGl, { MapContext } from "react-mapbox-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
 
 const Map = ReactMapboxGl({
@@ -16,9 +16,48 @@ const MapView = () => {
           width: "100vw",
         }}
       >
-        <Layer type="symbol" id="marker" layout={{ "icon-image": "marker-15" }}>
-          <Feature coordinates={[-0.481747846041145, 51.3233379650232]} />
-        </Layer>
+        <MapContext.Consumer>
+          {(map) => {
+
+            const data = {
+              "type": "FeatureCollection",
+              "features": [
+                  {
+                      "type": "Feature",
+                      "geometry": {
+                          "type": "LineString",
+                          "coordinates": [
+                              [
+                                  -122.019807,
+                                  45.632433
+                              ],
+                          ]
+                      }
+                  }
+              ]
+            }
+
+            map.addSource('trace', { type: 'geojson', data: data});
+            map.addLayer({
+              'id': 'trace',
+              'type': 'line',
+              'source': 'trace',
+              'paint': {
+              'line-color': 'yellow',
+              'line-opacity': 0.75,
+              'line-width': 5
+              }
+            });
+            map.panTo(data.features[0].geometry.coordinates[0]);
+            
+            map.on('click', async (e) => {
+              const {lng, lat} = e.lngLat
+              console.log([lng, lat])
+              data.features[0].geometry.coordinates.push([lng, lat]);
+              map.getSource('trace').setData(data);
+            })
+          }}
+        </MapContext.Consumer>
       </Map>
       ;
     </div>
