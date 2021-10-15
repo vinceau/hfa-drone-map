5331fb2cdad0483decbdbddc7a4a1ba2125e6f03
vinceau <vinceau@users.noreply.github.com>
2021-10-15 12:44:30 +0000

    Automated deployment: Fri Oct 15 12:44:30 UTC 2021 5d1c44eff9b96e0cffd1e89a8afe68099d98ee31

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.1afa6f969435dafb7b706ab8202515d6.js"
+  "/precache-manifest.1fc913c5068a058516261ca78340da54.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
