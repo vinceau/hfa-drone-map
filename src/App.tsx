6d13f1b5673d0778311ad29b074b119e33ea941d
Vince Au <vinceau09@gmail.com>
2021-10-15 21:37:09 +1100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.scss";
+
+import React from "react";
+import { hot } from "react-hot-loader/root";
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <div>Hello world!</div>
+    </div>
+  );
+};
+
+export default hot(App);
