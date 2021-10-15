5436d9fc8667a03c33044fe8a19806346cca6af0
Vince Au <vinceau09@gmail.com>
2021-10-16 09:49:04 +1100

    add linting configuration

---
+++ .eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+  parserOptions: {
+    ecmaVersion: 2020, // Allows for the parsing of modern ECMAScript features
+    sourceType: "module", // Allows for the use of imports
+    ecmaFeatures: {
+      jsx: true, // Allow parsing of JSX
+    },
+  },
+  ignorePatterns: ["/*.js"],
+  plugins: ["simple-import-sort", "react-hooks"],
+  extends: [
+    "plugin:react/recommended",
+    "eslint:recommended",
+    "plugin:import/errors",
+    "plugin:import/warnings",
+    // Enables eslint-plugin-prettier and eslint-config-prettier.
+    // This will display prettier errors as ESLint errors.
+    // Make sure this is always the last configuration in the extends array.
+    "plugin:prettier/recommended",
+  ],
+  rules: {
+    "react/prop-types": "off",
+    "react/display-name": "off",
+    "react/no-unescaped-entities": "warn",
+    "import/no-default-export": "error",
+    "import/no-named-as-default-member": "off",
+    "simple-import-sort/imports": "warn",
+    "simple-import-sort/exports": "warn",
+    "no-undef": "off",
+    "new-cap": "error",
+    "react-hooks/rules-of-hooks": "error", // Checks rules of Hooks
+    "react-hooks/exhaustive-deps": "warn", // Checks effect dependencies
+    curly: "error",
+  },
+};
