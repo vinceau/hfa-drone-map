8f15465ad77e312fc178e2c9787b11f06d72fe76
Vince Au <vinceau09@gmail.com>
2021-10-16 15:39:37 +1100

    feat: add rounding when generating csv

--- src/components/CsvDownloadButton.js
+++ src/components/CsvDownloadButton.js
@@ -1,16 +1,22 @@
 import React from "react";
 import { createDownloadableFile } from "../lib/createDownloadableFile";
 
+const ROUNDING_ACCURACY = 6;
+
 export const CsvDownloadButton = ({
   waypoints,
   filename = "data.csv",
   contentType = "data:text/csv;charset=utf-8,",
 }) => {
   let fileContents = "boatId,waypointNumber,latitude,longitude\n";
-  fileContents += waypoints.map((point, i) => `boatId,${i + 1},${point.lat},${point.long}`).join("\n");
+  fileContents += waypoints.map((point, i) => generateCsvEntryString("boatId", i, point)).join("\n");
   const onClick = () => {
     createDownloadableFile(fileContents, filename, contentType);
   };
 
   return <button onClick={onClick}>download</button>;
+};
+
+const generateCsvEntryString = (boatId, index, data) => {
+  return `${boatId},${index + 1},${data.lat.toFixed(ROUNDING_ACCURACY)},${data.long.toFixed(ROUNDING_ACCURACY)}`;
 };
